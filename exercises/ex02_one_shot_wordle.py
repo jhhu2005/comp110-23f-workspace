191e5f677f1b19f7fa3f68581f641f8203c9a94a
Jonathan Hu <jhhu@unc.edu>
2023-09-17 14:35:42 -0400

    Ex02 Progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,5 +1,5 @@
 """EX02 - One Shot Wordle"""
-__author__: "730664658"
+__author__= "730664658"
 
 secret_word: str = "python"
 guess = input (f"What is your {len(secret_word)}-letter guess? ")
@@ -13,22 +13,22 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 while len(guess) != len(secret_word):
-    #check for if the user's guess is 6 letters
+    # Check for if the user's guess is 6 letters
     guess = input (f"That was not {len(secret_word)} letters! Try again: ")
    
-    #if user guess is 6 letters then check if the word is right below
 
+# Below checks ff user guess is 6 letters then check which letters match
 if len(guess) == len(secret_word):
-    #if user's guess is 6 letters
+    #I f user's guess is 6 letters
     while guess_idx < len(secret_word):
         exists_anywhere = False 
-        #exists_anywhere = False serves to reset the search
-        #green box for correctly guessed letters of word and doesn't exist elsewhere
+        # Exists_anywhere = False serves to reset the search
+        # Green box for correctly guessed letters of word and doesn't exist elsewhere
         if guess[guess_idx] == secret_word[secret_word_idx]:
             emoji += GREEN_BOX
 
         else:
-            #if letter doesn't exist anywhere, or exists in another index, results in white box or yellow box
+            # If letter doesn't exist anywhere, or exists in another index, results in white box or yellow box
             alternate_idx = 0 
             while exists_anywhere==False and alternate_idx < len(secret_word):
                 if secret_word[alternate_idx] == guess[guess_idx]:
@@ -37,11 +37,11 @@ if len(guess) == len(secret_word):
                 alternate_idx += 1
             if not exists_anywhere:
                 emoji += WHITE_BOX
-        #runs through next index            
+        # Runs through next index            
         guess_idx += 1
         secret_word_idx += 1
 
-#prints the new emoji string
+# Prints the new emoji string
 print(emoji)
 if secret_word == guess:
     print ("Woo! You got it!")
