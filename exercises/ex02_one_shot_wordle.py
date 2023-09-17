d9d19ad50aa12cb7eb96060cefc097941a83fbf4
Jonathan Hu <jhhu@unc.edu>
2023-09-17 14:30:36 -0400

    Ex02 progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -15,15 +15,20 @@ YELLOW_BOX: str = "\U0001F7E8"
 while len(guess) != len(secret_word):
     #check for if the user's guess is 6 letters
     guess = input (f"That was not {len(secret_word)} letters! Try again: ")
-    #if user guess is 6 letters then check if the word is right
+   
+    #if user guess is 6 letters then check if the word is right below
 
 if len(guess) == len(secret_word):
+    #if user's guess is 6 letters
     while guess_idx < len(secret_word):
-        exists_anywhere = False
+        exists_anywhere = False 
+        #exists_anywhere = False serves to reset the search
+        #green box for correctly guessed letters of word and doesn't exist elsewhere
         if guess[guess_idx] == secret_word[secret_word_idx]:
             emoji += GREEN_BOX
 
         else:
+            #if letter doesn't exist anywhere, or exists in another index, results in white box or yellow box
             alternate_idx = 0 
             while exists_anywhere==False and alternate_idx < len(secret_word):
                 if secret_word[alternate_idx] == guess[guess_idx]:
@@ -32,11 +37,11 @@ if len(guess) == len(secret_word):
                 alternate_idx += 1
             if not exists_anywhere:
                 emoji += WHITE_BOX
-                    
+        #runs through next index            
         guess_idx += 1
         secret_word_idx += 1
 
-
+#prints the new emoji string
 print(emoji)
 if secret_word == guess:
     print ("Woo! You got it!")
