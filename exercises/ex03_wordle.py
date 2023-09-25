15dcc73c8f46ad4641740de57d100c0c736db8a2
Jonathan Hu <jhhu@unc.edu>
2023-09-25 17:10:26 -0400

    ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,12 +1,12 @@
 """EX03 - Structured Wordle."""
-__author__ : "730664658"
+__author__ = "730664658"
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 
 def contains_char(main_string: str, search_char: str) -> bool:
-    """Searches for character in string"""
+    """Searches for character in string."""
     assert len(search_char) == 1
     main_string_idx: int = 0
     # Returns true if the single character is found anywhere in the main string, if not return false.
@@ -17,18 +17,19 @@ def contains_char(main_string: str, search_char: str) -> bool:
             main_string_idx += 1 
     return False 
 
+
 def emojified(guess: str, secret: str) -> str:
-    """Creates string of emoji"""
+    """Creates string of emoji."""
     assert len(guess) == len(secret)
     emoji: str = ""
     secret_idx: int = 0
     guess_idx: int = 0
-    # Adds the box that corresponds to whether the current character of index is matching, existing in string, or not existing at all
+    # Adds the box that corresponds to whether the current character of index is matching, existing in string, or not existing at all.
     while (int(guess_idx)) < len(secret):
         if secret[secret_idx] == guess[guess_idx]:
             emoji += GREEN_BOX
         else: 
-            if contains_char(secret, guess[guess_idx]) == True:
+            if contains_char(secret, guess[guess_idx]):
                 emoji += YELLOW_BOX
             else: 
                 emoji += WHITE_BOX
@@ -36,26 +37,28 @@ def emojified(guess: str, secret: str) -> str:
         secret_idx += 1
     return emoji
 
+
 def input_guess(input_guess_len: int) -> str:
-    """Prompts for user input"""
-    # Asks for user input and returns if matches the expected string length
+    """Prompts for user input."""
+    # Asks for user input and returns if matches the expected string length.
     guess: str = input("Enter a " + str(input_guess_len) + " character word: ")
     while input_guess_len != len(guess):
         guess = input("That wasn't " + str(input_guess_len) + " chars! Try again: ")
     return guess
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
     turn_number: int = 0
     won: bool = False
     
-    # Loops through the turn numbers and stops loop if turns goes above 6
+    # Loops through the turn numbers and stops loop if turns goes above 6.
     while turn_number < 6 and (not won):
         turn_number += 1
         print("=== Turn " + str(turn_number) + "/6 ===")
         # Calls input_guess function
         guess: str = input_guess(5)
-        # Calls emojified function to display correct characters/existing/wrong characters
+        # Calls emojified function to display correct characters/existing/wrong characters.
         print(emojified(guess, "codes"))
         if guess == "codes":
             won = True
@@ -64,21 +67,6 @@ def main() -> None:
     else:
         print("X/6 - Sorry, try again tomorrow!")
 
+
 if __name__ == "__main__":
     main()
-
-
-    
-    
-    
-    
-
-
-
-
-
-
-
-            
-        
-        
